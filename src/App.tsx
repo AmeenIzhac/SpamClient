a899ed3580950bb4f9eb068d2488017965f4e7f1
AmeenIzhac <ameen.izhac@gmail.com>
2023-06-05 05:02:54 +0100

    [Dev] Requests now made to server, buttons also trigger server but do not yet receive

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,83 @@
+import { useState, useEffect } from "react";
 import ReportWidget from "./components/ReportWidget";
 import axios from "axios";
 
+const SERVER = "http://localhost:4000";
+
+interface ReportsData {
+  size: number;
+  nextOffset: String;
+  elements: Report[];
+}
+
+interface Report {
+  id: string;
+  source: string;
+  sourceIdentityId: string;
+  reference: Reference;
+  state: string;
+  payload: Payload;
+  created: string;
+}
+
+interface Reference {
+  referenceId: string;
+  referenceType: string;
+}
+
+interface Payload {
+  source: string;
+  reportType: string;
+  message: string | null;
+  reportId: string;
+  referenceResourceId: string;
+  referenceResourceType: string;
+}
+
 function App() {
+  const [reports, setReports] = useState<Report[]>([]);
+
+  useEffect(() => {
+    function fetchReports() {
+      axios
+        .get(`${SERVER}/reports`)
+        .then((response) => {
+          parseReports(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    fetchReports();
+  }, []);
+
+  function parseReports(reportsData: ReportsData) {
+    const reports: Report[] = reportsData.elements;
+    const openReports = [];
+    for (let i = 0; i < reports.length; i++) {
+      if (reports[i].state === "OPEN") {
+        openReports.push(reports[i]);
+      }
+    }
+    setReports(openReports);
+  }
+
   return (
     <div>
       <h1>Reports</h1>
-      <ReportWidget />
-      <ReportWidget />
-      <ReportWidget />
-      <ReportWidget />
-      <ReportWidget />
+      {reports.map((report: Report) => (
+        <ReportWidget
+          key={report.id}
+          id={report.id}
+          type={report.payload.reportType}
+          state={report.state}
+          message={
+            report.payload.message
+              ? report.payload.message.substring(0, 20)
+              : ""
+          }
+        />
+      ))}
     </div>
   );
 }
--- src/components/ReportWidget.tsx
+++ src/components/ReportWidget.tsx
@@ -1,18 +1,48 @@
 import styles from "../styles/ReportWidget.module.scss";
+import axios from "axios";
 
-function ReportWidget() {
+const SERVER = "http://localhost:4000";
+
+interface types {
+  id: string;
+  type: string;
+  state: string;
+  message: string;
+}
+
+function ReportWidget({ id, type, state, message }: types) {
+  function handleBlock(id: string) {
+    axios
+      .put(`${SERVER}/reports/${id}`, {
+        ticketState: "BLOCKED",
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function handleResolve(id: string) {
+    axios
+      .put(`${SERVER}/reports/${id}`, {
+        ticketState: "RESOLVED",
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <div className={styles.main}>
       <div className={styles.content}>
-        <p>Id: {}</p>
-        <p>Type: {}</p>
-        <p>State: {}</p>
-        <p>Message: {}</p>
+        <p>Id: {id}</p>
+        <p>Type: {type}</p>
+        <p>State: {state}</p>
+        <p>Message: {message}</p>
         <p className={styles.linkMock}>Details</p>
       </div>
       <div className={styles.actions}>
-        <button>block</button>
-        <button>resolve</button>
+        <button onClick={() => handleBlock(id)}>block</button>
+        <button onClick={() => handleResolve(id)}>resolve</button>
       </div>
     </div>
   );
